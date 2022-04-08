247a13b8e893ea8b9770284dfe1e509bf1b721d3
cri732 <cri732@hotmail.fr>
2022-04-08 09:52:20 +0200

    VERSION 0 : Codes à tester

---
+++ import.py
@@ -0,0 +1,69 @@
+import requests
+import json
+import unicodedata
+
+# LIENS NON JSON + MAUVAIS LIEN les chats
+# open_quizz_db_data = (
+#     ("Animaux", "Les chats", "https://www.kiwimeeeeee.com/oqdb/files/1050828847/OpenQuizzDB_050/openquizzdb_50.json"),
+#     ("Arts", "Musée du Louvre", "https://www.kiwime.com/oqdb/files/1086624389/OpenQuizzDB_086/openquizzdb_86.json"),
+#     ("Bande dessinnée", "Tintin", "https://www.kiwime.com/oqdb/files/2124627384/OpenQuizzDB_124/openquizzdb_124.json"),
+#     ("Cinéma", "Alien", "https://www.kiwime.com/oqdb/files/3241454997/OpenQuizzDB_241/openquizzdb_241.json"),
+#     ("Cinéma", "Star wars", "https://www.kiwime.com/oqdb/files/1090993427/OpenQuizzDB_090/openquizzdb_90.json"),
+# )
+
+# LIENS OK LE 20/03/2022
+open_quizz_db_data = (
+    ("Animaux", "Les chats", "https://www.kiwime.com/oqdb/files/1050288832/OpenQuizzDB_050/openquizzdb_50.json"),
+    ("Arts", "Musée du Louvre", "https://www.kiwime.com/oqdb/files/1086665427/OpenQuizzDB_086/openquizzdb_86.json"),
+    ("Bande dessinnée", "Tintin", "https://www.kiwime.com/oqdb/files/2124627594/OpenQuizzDB_124/openquizzdb_124.json"),
+    ("Cinéma", "Alien", "https://www.kiwime.com/oqdb/files/3241985774/OpenQuizzDB_241/openquizzdb_241.json"),
+    ("Cinéma", "Star wars", "https://www.kiwime.com/oqdb/files/1090683544/OpenQuizzDB_090/openquizzdb_90.json"),
+)
+
+
+def strip_accents(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
+
+
+def get_quizz_filename(categorie, titre, difficulte):
+    return strip_accents(categorie).lower().replace(" ", "") + "_" + strip_accents(titre).lower().replace(" ", "") + "_" + strip_accents(difficulte).lower().replace(" ", "") + ".json"
+
+
+def generate_json_file(categorie, titre, url):
+    out_questionnaire_data = {"categorie": categorie, "titre": titre, "questions": []}
+    out_questions_data = []
+    # Gestion si mauvaise url
+    try:
+        response = requests.get(url)
+    except:
+        print(f"Erreur avec le lien: {url}")
+    else:
+        # Gestion si lien n'est pas fichier json
+        try:
+            data = json.loads(response.text)
+            all_quizz = data["quizz"]["fr"]
+            for quizz_title, quizz_data in all_quizz.items():
+                out_filename = get_quizz_filename(categorie, titre, quizz_title)
+                print(out_filename)
+                out_questionnaire_data["difficulte"] = quizz_title
+                for question in quizz_data:
+                    question_dict = {}
+                    question_dict["titre"] = question["question"]
+                    question_dict["choix"] = []
+                    for ch in question["propositions"]:
+                        question_dict["choix"].append((ch, ch==question["réponse"]))
+                    out_questions_data.append(question_dict)
+                out_questionnaire_data["questions"] = out_questions_data
+                out_json = json.dumps(out_questionnaire_data)
+
+                file = open(out_filename, "w")
+                file.write(out_json)
+                file.close()
+                print("end")
+        except:
+            print(f"Erreur dans la désérialisation du lien: {url} - Questionnaire {titre} non généré")
+
+
+for quizz_data in open_quizz_db_data:
+    generate_json_file(quizz_data[0], quizz_data[1], quizz_data[2])
+
---
+++ questionnaire.py
@@ -0,0 +1,131 @@
+# Import de json pour charger les fichiers de questionnaires générés avec script import
+import json
+import sys
+
+
+class Question:
+    def __init__(self, titre, choix, bonne_reponse):
+        self.titre = titre
+        self.choix = choix
+        self.bonne_reponse = bonne_reponse
+
+    def from_json_data(data):
+        # Définir les choix de réponses
+        choix = [i[0] for i in data["choix"]]
+        # Définir la bonne réponse
+        bonne_reponse = [i[0] for i in data["choix"] if i[1] == True]
+        # Gestion erreur s'il y a plusieurs bonnes réponses ou s'il n'y en a aucune
+        if len(bonne_reponse) != 1:
+            return None
+        question = Question(data["titre"], choix, bonne_reponse[0])
+        return question
+
+    def poser(self, num_question, nb_questions):
+        print(f"QUESTION {num_question} / {nb_questions}")
+        print("  " + self.titre)
+        for i in range(len(self.choix)):
+            print("  ", i+1, "-", self.choix[i])
+
+        print()
+        resultat_response_correcte = False
+        reponse_int = Question.demander_reponse_numerique_utlisateur(1, len(self.choix))
+        if self.choix[reponse_int-1].lower() == self.bonne_reponse.lower():
+            print("Bonne réponse")
+            resultat_response_correcte = True
+        else:
+            print("Mauvaise réponse")
+            print(f"La bonne réponse était: {self.bonne_reponse}")
+            
+        print()
+        return resultat_response_correcte
+
+    def demander_reponse_numerique_utlisateur(min, max):
+        reponse_str = input("Votre réponse (entre " + str(min) + " et " + str(max) + ") :")
+        try:
+            reponse_int = int(reponse_str)
+            if min <= reponse_int <= max:
+                return reponse_int
+
+            print("ERREUR : Vous devez rentrer un nombre entre", min, "et", max)
+        except:
+            print("ERREUR : Veuillez rentrer uniquement des chiffres")
+        return Question.demander_reponse_numerique_utlisateur(min, max)
+
+
+class Questionnaire:
+    def __init__(self, questions, categorie, titre, difficulte):
+        self.questions = questions
+        self.categorie = categorie
+        self.titre = titre
+        self.difficulte = difficulte
+
+    def from_json_data(data):
+        # Récupérer les questions
+        questionnaire_data_questions = data["questions"]
+        # Mise en forme des questions : peut générer None (voir from_data_json class Question)
+        questions = [Question.from_json_data(i) for i in questionnaire_data_questions]
+        # Suppression des questions None
+        questions = [i for i in questions if i]
+        return Questionnaire(questions, data["categorie"], data["titre"], data["difficulte"])
+
+    def from_json_file(filename):
+        try:
+            # Charger un fichier json
+            with open(filename, "r", encoding="utf-8") as f:
+                questionnaire_data = json.load(f)
+        except:
+            print("Erreur lors de l'ouverture ou la lecture du fichier")
+            return None
+        # Créer le questionnaire
+        return Questionnaire.from_json_data(questionnaire_data)
+
+    def lancer(self):
+        score = 0
+        nb_questions = len(self.questions)
+        # Afficher infos questionnaire
+        print("----------")
+        print(f"QUESTIONNAIRE: {self.titre}")
+        print(f"    Catégorie: {self.categorie}")
+        print(f"    Difficulté: {self.difficulte}")
+        print(f"    Nombre de questions: {nb_questions}")
+        print("----------")
+        for i in range(nb_questions):
+            question = self.questions[i]
+            if question.poser(i + 1, nb_questions):
+                score += 1
+        print("Score final :", score, "sur", nb_questions)
+        return score
+
+
+# -------------------- PHASE PROD : TOUS LES QUESTIONNAIRES --------------------
+# Exécuter cette partie de code que si le nom du fichier lancé est le même que le nom dans le main
+if __name__ == "__main__":
+    # Questionnaire.from_json_file(animaux_leschats_confirme.json).lancer()
+    if len(sys.argv) < 2:
+        print("ERREUR: Vous devez spécifié le nom du fichier json à charger")
+        exit(0)
+    json_filename = sys.argv[1]
+    questionnaire = Questionnaire.from_json_file(json_filename)
+    if questionnaire:
+        questionnaire.lancer()
+
+
+# -------------------- PHASE TESTS : UN SEUL QUESTIONNAIRE --------------------
+# # Tester première question
+# # Charger un fichier json
+# with open('animaux_leschats_debutant.json', "r", encoding="utf-8") as f:
+#     questionnaire_data = json.load(f)
+# # Récupérer les questions
+# questionnaire_data_questions = questionnaire_data["questions"]
+# # Mise en forme de la question
+# question = Question.from_json_data(questionnaire_data_questions[0])
+# # Poser une seule question
+# question.poser()
+
+# Tester lancement questionnaire : Ancienne méthode
+# # Charger un fichier json
+# with open('animaux_leschats_debutant.json', "r", encoding="utf-8") as f:
+#     questionnaire_data = json.load(f)
+# # Créer et lancer le questionnaire
+# Questionnaire.from_json_data(questionnaire_data).lancer()
+# print()
